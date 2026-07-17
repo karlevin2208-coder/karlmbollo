ffa10faf79b49ef6fb8ed907e5e4c0a41faecd66
karlevin2208-coder <karlevin2208@gmail.com>
2026-07-17 18:02:35 +0500

    Add files via upload

---
+++ Types of data.py
@@ -0,0 +1,22 @@
+
+
+nom = "Mbollo"
+prenom = "Karl Kevin"
+age = 29
+taille = 188
+est_etudiant = True
+
+# Afficher les valeurs des cinq variables
+print(f"Nom: {nom}")
+print(f"Prenom: {prenom}")
+print(f"Age: {age}")
+print(f"Taille: {taille}")
+print(f"Est etudiant: {True}")
+
+# Afficher les types des cinq variables
+print(f"Type nom: {type(nom)}")
+print(f"Type prenom: {type(prenom)}")
+print(f"Type age: {type(age)}")
+print(f"Type taille: {type(taille)}")
+print(f"Type est etudiant: {type(est_etudiant)}")
+
