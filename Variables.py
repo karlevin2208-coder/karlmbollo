de4e922fbd842db9da43c50bd8af4f1a77e19649
karlevin2208-coder <karlevin2208@gmail.com>
2026-07-17 17:35:22 +0500

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,34 @@
+
+
+livre = "Homelander"
+print(livre)
+livre = "Mortal Kombat"
+print(livre)
+age = 256
+print(age)
+age2 = 10
+age2 = age
+print(age2)
+
+nom = "Mbollo"
+prenom = "Karl Kevin"
+age = 30
+
+print(f"Bonjour, je m'appelle {prenom} {nom} et j'ai {age} ans.")
+
+age = 25
+nom = "KARL"
+print("Je m'appelle " + nom + " et j'ai " + str(age) + "ans.")
+
+nourriture_preferee = "Glace"
+nouvelle_nourriture_preferee = "Pizza"
+nourriture_preferee = "Pates"
+print(nourriture_preferee)
+
+nom = "Mbollo"
+prenom = "Karl Kevin"
+age = 29
+print(f"Je m'appelle {nom} {prenom} et j'ai {age} ans actuellement.")
+age = age + 10 
+print(f"Je m'appelle {nom} {prenom} et j'ai plutot {age} ans.")
+
